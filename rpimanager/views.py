4c1d3e8c1df1f8c00637752c859389304af10cc9
bogdan <bogdan.blidarescu@gmail.com>
2018-02-11 21:00:44 +0200

    ewgwertyh

--- rpimanager/views.py
+++ rpimanager/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from scada10.rpimanager import models
+from rpimanager import models
 # Create your views here.
 
 def listallpoles(request):
