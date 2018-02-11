08f969a1405d0feb8c80c5ad30d40d36c5cf0633
bogdan <bogdan.blidarescu@gmail.com>
2018-02-11 21:27:50 +0200

    json

--- rpimanager/views.py
+++ rpimanager/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rpimanager.models import Pole
+from django.core import serializers
 import json
 # Create your views here.
 
 def listallpoles(request):
-    polelist = Pole.objects.all().values()
+    polelist = Pole.objects.all()
     # context = {'pole_list': polelist}
     # return render(request, 'pole_overview.html', context)
-    dump = json.dumps(polelist)
+    dump = serializers.serialize('json', polelist)
 
     return Response(dump, status=200)
 
