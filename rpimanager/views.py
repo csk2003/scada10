13b3198afe138826ce6645975e8ffa136a672cc6
bogdan <bogdan.blidarescu@gmail.com>
2018-02-11 21:08:51 +0200

    werthreth

--- rpimanager/views.py
+++ rpimanager/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from rpimanager import models
+from rpimanager.models import Pole
 # Create your views here.
 
 def listallpoles(request):
