472e0020d7497a0760dab8a814f62aea704decb4
bogdan <bogdan.blidarescu@gmail.com>
2018-02-11 20:59:29 +0200

    url

--- rpimanager/urls.py
+++ rpimanager/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path('/^$', views.listallpoles)
-]
+# urlpatterns = [
+#     path('/^$', views.listallpoles)
+# ]
