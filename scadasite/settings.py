00081d4efa7f8d315d363ecdc61566cd25a81a31
bogdan <bogdan.blidarescu@gmail.com>
2018-02-11 19:10:01 +0200

    ergwerg

--- scadasite/settings.py
+++ scadasite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = ['rpimanager'
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django-extensions'
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
