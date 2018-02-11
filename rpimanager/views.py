dfbe259a84949769c956e439c2f673bf2838d304
bogdan <bogdan.blidarescu@gmail.com>
2018-02-11 21:15:01 +0200

    werthertyuh

--- rpimanager/views.py
+++ rpimanager/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rpimanager.models import Pole
+import json
 # Create your views here.
 
 def listallpoles(request):
 	polelist = Pole.objects.all().values()
 	# context = {'pole_list': polelist}
 	# return render(request, 'pole_overview.html', context)
-	return polelist
+	dump = json.dumps(polelist)
+    return HttpResponse(dump, content_type='application/json')
 
