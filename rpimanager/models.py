c157fcf8f8d8c803fec3ed2caa8740c3aa03beb3
bogdan <bogdan.blidarescu@gmail.com>
2018-02-11 17:13:52 +0200

    fixed some models

--- rpimanager/models.py
+++ rpimanager/models.py
@@ -14,53 +14,51 @@ class Pole(models.Model):
 	#pole failure is given by the absence of a sensor or the rpi
 	def pole_failure(self):
 		if not Raspbery_Health.state_suspended:
-			is_enabled = False
-			alarm.log_alarm(raspbery_suspended) 
+			self.is_enabled = False
+			Alarm.objects.create(pole_id=self.id,verbiage='raspbery_suspended') 
 		elif  not Raspbery_Health.wind_sensor_chk:
-			is_enabled = False
-			alarm.log_alarm(wind_sensor_fail)
+			self.is_enabled = False
+			Alarm.objects.create(pole_id=self.id,verbiage='wind_sensor_fail')
 		elif not Raspbery_Health.humidity_sensor_chk: 
-			is_enabled = False
-			alarm.log_alarm(humidity_sensor_fail)
+			self.is_enabled = False
+			Alarm.objects.create(pole_id=self.id,verbiage='humidity_sensor_fail')
 		elif not Raspbery_Health.water_debit_chk:
-			is_enabled = False
-			alarm.log_alarm(water_debit_fail)
+			self.is_enabled = False
+			Alarm.objects.create(pole_id=self.id,verbiage='water_debit_fail')
 		elif not Raspbery_Health.thermometer_chk:
-			is_enabled = False
-			alarm.log_alarm(thermo_fail)
-
+			self.is_enabled = False
+			Alarm.objects.create(pole_id=self.id,verbiage='thermo_fail')
 
 #the pole cone of influence. Weather data
 
 class Cone(models.Model):
-	temperature = models.FloatField()
-	humidity = models.FloatField()
-	wind_speed = models.IntegerField()
-	water_debit = models.IntegerField()
-	size_of_cone = models.IntegerField() 
+	temperature = models.FloatField(default=0)
+	humidity = models.FloatField(default=0)
+	wind_speed = models.PositiveIntegerField(default=0)
+	water_debit = models.PositiveIntegerField(default=0)
+	size_of_cone = models.PositiveIntegerField(default=0) 
+	pole = models.OneToOneField(
+        Pole,
+        on_delete=models.SET_NULL
+    )
 
 #alarm class. if pole sensor missing or values out of boundry or if colision of cone
 
 class Alarm(models.Model):
-	index = models.IntegerField()
-	id_alarm = models.IntegerField()
-	alarm_id_pole = models.ForeignKey(id_pole)
+	pole = models.ForeignKey(Pole)
 	verbiage = models.TextField(blank=True)
 
 	#raise alarm if pole is affected
-	def log_alarm(self, failure):
-		alarm_verbatim = {'raspbery_suspended': "General Failure Detected, RPI is suspended",
+	def log_alarm(self):
+		ALARM_VERBATIM = {'raspbery_suspended': "General Failure Detected, RPI is suspended",
 		'wind_sensor_fail': "Check wind sensor", 
 		'humidity_sensor_fail': "Check humidity sensor"
 		'water_debit_fail': "Check water debit sensor"
 		'thermo_fail': "Thermometer failure"
 		 }
 
-		 failure_verbiage = alarm_verbatim[failure]
+		 failure_verbiage = ALARM_VERBATIM.get(self.verbiage, default='Alarm not found!')
 		 return failure_verbiage
-
-
-
 
 #raspbery model
 
